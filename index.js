bad2a1dbbb79c72aaca90dfdb26692f3253ab8c6
handuy <duy@techmaster.vn>
2020-07-31 09:07:04 +0700

    change route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+app.get('/about', function (req, res) {
   res.send('Hello');
 });
 app.listen(3000, function () {
