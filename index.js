949e8d27c32e5dd0b4cb9278a8eeb5afec79b14a
handuy <duy@techmaster.vn>
2020-08-01 08:47:43 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-  res.send('Hello');
+  res.send('Hello node');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
