830a7296e6760d2eb85a11224500753dfdb6df71
handuy <duy@techmaster.vn>
2020-08-04 17:18:12 +0700

    add route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/about', function (req, res) {
-  res.send('Hello');
+  res.send('Hello 123456');
 });
 app.get('/contact', function (req, res) {
   res.send('<h1>haru</h1>');
