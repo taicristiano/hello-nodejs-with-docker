0c041969a348d507c3b12a7af799f5bbad2d3bbd
handuy <duy@techmaster.vn>
2020-08-04 15:45:05 +0700

    thêm route

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/about', function (req, res) {
   res.send('Hello world');
 });
+app.get('/', function (req, res) {
+  res.send('<h1>Hura</h1>');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
