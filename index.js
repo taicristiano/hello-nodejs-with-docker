8876e0de6f8264cf6825a28d7dc775d61001bf27
handuy <duy@techmaster.vn>
2020-10-06 08:42:23 +0700

    add nodemon

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ app.get('/about', function (req, res) {
   res.send('Hello 123');
 });
 app.get('/contact', function (req, res) {
-  res.send('<h1>haru</h1>');
+  res.send('<h1>1234567890 this is from Hanoi</h1>');
 });
 app.get('/', function (req, res) {
-  res.send('<h1>Hura</h1>');
+  res.send('<h1>Hura Haru Yahoo Google.com</h1>');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
